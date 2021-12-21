cff969276d372a249004e94545bc863a72d94b90
Jonathan Bell <jon@jonbell.net>
2021-12-21 02:24:39 +0000

    .

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const nameToGreet = core.getInput('include_branches');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("workflow_runs", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+    const nameToGreet = core.getInput('include_branches');
+    console.log(`Hello ${nameToGreet}!`);
+    //This branch 
+    const branch_triggering = github.context.payload.ref; //"refs/head/main"
+
+
+    const time = (new Date()).toTimeString();
+    core.setOutput("workflow_runs", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const context = JSON.stringify(github.context, undefined, 2)
+    console.log(`The context: ${context}`);
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
