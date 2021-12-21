c27083bba2813576ef0059b1f19272703de9e410
Jonathan Bell <jon@jonbell.net>
2021-12-21 02:36:07 +0000

    try to list wf runs

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-    const nameToGreet = core.getInput('include_branches');
-    console.log(`Hello ${nameToGreet}!`);
-    //This branch 
-    const branch_triggering = github.context.payload.ref; //"refs/head/main"
-    const workflow = github.context.workflow; //"dev"
-    const runID = github.context.runId;
-
-    const octokit = github.getOctokit(core.getInput("GITHUB_TOKEN"));
-    const res = await octokit.rest.actions.listWorkflowRuns({workflow_id: workflow, branch: branch_triggering})
-    const time = (new Date()).toTimeString();
-    core.setOutput("workflow_runs", time);
-    const dbg = JSON.stringify(res, undefined, 2)
-    console.log(`The thing: ${dbg}`);
-} catch (error) {
-    core.setFailed(error.message);
+async function action(){
+    try {
+        const nameToGreet = core.getInput('include_branches');
+        console.log(`Hello ${nameToGreet}!`);
+        //This branch 
+        const branch_triggering = github.context.payload.ref; //"refs/head/main"
+        const workflow = github.context.workflow; //"dev"
+        const runID = github.context.runId;
+    
+        const octokit = github.getOctokit(core.getInput("GITHUB_TOKEN"));
+        const res = await octokit.rest.actions.listWorkflowRuns({workflow_id: workflow, branch: branch_triggering})
+        const time = (new Date()).toTimeString();
+        core.setOutput("workflow_runs", time);
+        const dbg = JSON.stringify(res, undefined, 2)
+        console.log(`The thing: ${dbg}`);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+action();
