d05b9e47374dfb5ed914c2b1cfd6ae88b16a23c5
Jonathan Bell <jon@jonbell.net>
2021-12-21 15:09:44 +0000

    add other branches

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function getLastNWorkflowRuns(octokit, workflow_id, branch, numRuns){
     runs_this_branch.sort((a, b) => {
         return b.created_at.localeCompare(a.created_at);
     });
-    return runs_this_branch.slice(0, numRuns);
+    return {branch: runs_this_branch.slice(0, numRuns)};
 }
 async function action() {
     try {
@@ -31,14 +31,25 @@ async function action() {
         const branch_triggering = thisWfRun.data.head_branch; 
         const thisWfID = thisWfRun.data.workflow_id;
 
+        const branchesToCheck = [branch_triggering];
+        const results = {};
+        if(include_branches){
+            for(let s of include_branches.split(",")){
+                s = s.trim();
+                if(!branchesToCheck.includes(s)){
+                    branchesToCheck.push(s);
+                }
+            }
+        }
+        const byBranch = await Promise.all(branchesToCheck.map(branch => getLastNWorkflowRuns(octokit, thisWfID, branch, number_runs + (branch == branch_triggering ? 1 : 0))));
+
         console.log("WF: " + workflow)
         console.log("BranchTrigger: " + branch_triggering)
        
         const time = (new Date()).toTimeString();
         core.setOutput("workflow_runs", time);
 
-        const res = await getLastNWorkflowRuns(octokit, thisWfID, "main", number_runs);
-        const dbg2 = JSON.stringify(res, undefined, 2)
+        const dbg2 = JSON.stringify(byBranch, undefined, 2)
         console.log(`response: ${dbg2}`);
 
     } catch (error) {
